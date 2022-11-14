f4f318bf7200cbcbfd35847268043b1b75f948d1
SimasSaf <simas.safr@gmail.com>
2022-11-14 11:57:56 +0100

    SEP4SD-5 Added the folder Entities and the User entity

---
+++ RESTServer/Entities/User.cs
@@ -0,0 +1,29 @@
+﻿namespace WebApplication1.Entities;
+
+public class User
+{
+    
+
+    public User(string firstName, string lastName, string username, string password, int age, string country, char sex)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.age = age;
+        this.country = country;
+        this.sex = sex;
+    }
+
+    public User()
+    {
+    }
+
+    public string firstName { get; set; }
+    public string lastName { get; set; }
+    public string username { get; set; }
+    public string password { get; set; }
+    public int age { get; set; }
+    public string country { get; set; }
+    public char sex { get; set; }
+}
