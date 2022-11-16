0c242d5e59df174cb8518ee60439b053ebc8a09d
Adi <militaruadriancristian@gmail.com>
2022-11-16 23:50:05 +0100

    add test env to proj

---
+++ NUnitTest/UserNUnitTest.cs
@@ -0,0 +1,22 @@
+﻿namespace NUnitTest;
+
+using NUnit.Framework;
+
+public class UserNUnitTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+    }
+    
+    [Test]
+    public void GetingPostByUsername()
+    {
+        
+    }
+}
