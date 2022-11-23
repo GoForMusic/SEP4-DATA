99f6b6ce24d5bb498e3dc305d3b108ce3ae75205
Mvycas <martynas.vycas@hotmail.com>
2022-11-23 22:02:03 +0100

    SEP4SD-19

---
+++ RESTServer/Services/ISensorsService.cs
@@ -0,0 +1,13 @@
+﻿using Entity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Services;
+
+public interface ISensorsService
+{
+    Task<ICollection<Sensors>> GetAllSensorsDataAsync();
+    Task<ICollection<Sensors>> GetSensorDataByDate(DateTime startDate, DateTime endDate);
+    Task<Sensors> GetSensorsDataByIdAsync(string id);
+    Task<Sensors> AddSensorsDataAsync(Sensors sensors);
+    Task<Sensors> RemoveSensorsDataAsync();
+}
