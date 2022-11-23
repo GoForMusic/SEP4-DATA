01ed664dd6418814b1f54eeace748942f672fa82
Mvycas <martynas.vycas@hotmail.com>
2022-11-23 22:02:03 +0100

    SEP4SD-19

---
+++ RESTServer/Controllers/SensorsController.cs
@@ -0,0 +1,93 @@
+﻿using Entity;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Services;
+
+namespace Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SensorsController : ControllerBase
+{
+    private ISensorsService _sensorsService;
+
+    public SensorsController(ISensorsService sensorsService)
+    {
+        _sensorsService = sensorsService;
+    }
+
+        [HttpGet]
+        [Route("/all")]
+        public async Task<ActionResult<ICollection<Sensors>>> GetAllSensorsDataAsync()
+        {
+            try
+            {
+                ICollection<Sensors> sensors = await _sensorsService.GetAllSensorsDataAsync();
+                return Ok(sensors);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("/all/date")]
+        public async Task<ActionResult<ICollection<Sensors>>> GetSensorDataByDate([FromQuery] DateTime startDate,
+            [FromQuery] DateTime endDate)
+        {
+            try
+            {
+                ICollection<Sensors> sensors = await _sensorsService.GetSensorDataByDate(startDate, endDate);
+                return Ok(sensors);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<ActionResult<Sensors>> GetSensorsDataByIdAsync([FromQuery] string id)
+        {
+            try
+            {
+                Sensors sensors = await _sensorsService.GetSensorsDataByIdAsync(id);
+                return Ok(sensors);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Sensors>> AddSensorsDataAsync([FromBody] Sensors sensors)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                Sensors localSensors = await _sensorsService.AddSensorsDataAsync(sensors);
+                return Created($"/{localSensors.Id}", localSensors);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+        
+        [HttpDelete]
+        [Route("/delete/all")]
+        public async Task<ActionResult<Sensors>> RemoveSensorsDataAsync() {
+            try {
+                await _sensorsService.RemoveSensorsDataAsync();
+                return Ok();
+            } catch (Exception e) {
+                Console.WriteLine(e);
+                return StatusCode(500, e.Message);
+            }
+        }
+    }
---
+++ RESTServer/Services/SensorsService.cs
@@ -0,0 +1,72 @@
+﻿using Entity;
+namespace WebApplication1.Services;
+
+public class SensorsService : ISensorsService
+{
+    private ISensorsDAO _sensorsDao; 
+    
+    public SensorsService(ISensorsDAO sensorsDao)
+    {
+        _sensorsDao = sensorsDao;
+    }
+    
+    public async Task<ICollection<Sensors>> GetAllSensorsDataAsync()
+    {
+        try
+        {
+            return await _sensorsDao.GetAllSensorsDataAsync(); // DAO SHOULD RETURN LIST/ARRAY;
+        }
+        catch (Exception e)
+        {
+            throw new Exception(e.Message);
+        }
+    }
+
+    public async Task<ICollection<Sensors>> GetSensorDataByDate(DateTime startDate, DateTime endDate)
+    {
+        try
+        {
+            return await _sensorsDao.GetSensorDataByDateAsync(startDate, endDate); // Query logic should be in DAO implementation
+        }
+        catch (Exception e)
+        {
+            throw new Exception(e.Message);
+        }
+    }
+    
+    public async Task<Sensors> GetSensorsDataByIdAsync(string id)
+    {
+        try
+        {
+            return await _sensorsDao.GetSensorDataByIdAsync(id); 
+        }
+        catch (Exception e)
+        {
+            throw new Exception(e.Message);
+        }
+    }
+
+    public async Task<Sensors> AddSensorsDataAsync(Sensors sensors)
+    {
+        try
+        {
+            return await _sensorsDao.AddSensorsDataAsync(sensors);
+        }
+        catch (Exception e)
+        {
+            throw new Exception(e.Message);
+        }
+    }
+
+    public async Task<Sensors> RemoveSensorsDataAsync()
+    {
+        try
+        {
+            return await _sensorsDao.RemoveAllSensorData();
+        }
+        catch (Exception e)
+        {
+            throw new Exception(e.Message);
+        }
+    }
+}
