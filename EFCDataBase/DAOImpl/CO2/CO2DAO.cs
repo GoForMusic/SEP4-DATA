e004411d475758e0a831648af4c12375f169ae8c
VassilevEmil <emilvassilev5@abv.bg>
2022-11-26 16:35:07 +0100

    CO2 Dao done

---
+++ EFCDataBase/DAOImpl/CO2/CO2DAO.cs
@@ -0,0 +1,50 @@
+﻿using Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace EFCDataBase.DAOImpl.CO2;
+
+public class CO2DAO : ICO2DAO
+{
+
+    private readonly DBContext database;
+
+    public async Task<Sensors> GetCO2Async(string id)
+    {
+        return await database.Sensors.FirstAsync(t => t.Co2.Equals(id));
+    }
+
+    public async Task DeleteCO2Async(string id)
+    {
+        Sensors sensor = await database.Sensors.FirstAsync(s => s.Id.Equals(id));
+        if (sensor is null)
+        {
+            throw new Exception("ERROR:  Sensor does not exist");
+        }
+        database.Sensors.Remove(sensor);
+        await database.SaveChangesAsync();
+        
+    }
+
+    public async Task UpdateCO2Async(Sensors sensor)
+    {
+        database.Sensors.Update(sensor);
+        await database.SaveChangesAsync();
+    }
+
+    public async Task<Sensors> AddSensorAsync(Sensors sensor)
+    {
+        try
+        {
+            EntityEntry<Sensors> newSensor = await database.Sensors.AddAsync(sensor);
+            await database.SaveChangesAsync();
+            return newSensor.Entity;
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine(e+" "+ e.StackTrace);
+        }
+
+        return sensor;
+    }
+}
---
+++ EFCDataBase/DAOImpl/CO2/ICO2DAO.cs
@@ -0,0 +1,11 @@
+﻿using Entity;
+
+namespace EFCDataBase.DAOImpl.CO2;
+
+public interface ICO2DAO
+{
+    public Task<Sensors> GetCO2Async(string id);
+    public Task DeleteCO2Async(string id);
+    public Task UpdateCO2Async(Sensors sensor);
+    public Task<Sensors> AddSensorAsync(Sensors sensor);
+}
