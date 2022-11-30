27e7f822767c4c422035068e0a06782e1b6e65b3
Mvycas <martynas.vycas@hotmail.com>
2022-11-30 20:30:33 +0100

    Box dao created.

---
+++ EFCDataBase/DAOImpl/IBoxDao.cs
@@ -0,0 +1,12 @@
+﻿using Entity;
+
+namespace EFCDataBase.DAOImpl;
+
+public interface IBoxDao
+{
+    Task<List<Box>> GetBoxesAsync();
+    Task<Box> GetBoxAsync(string id);
+    Task DeleteBoxAsync(string id);
+    Task UpdateBoxAsync(Box box);
+    Task<Box> AddBoxAsync(Box box);
+}
