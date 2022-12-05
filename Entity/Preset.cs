8128e968db298b134a1c3a5260abd595cdbcf06c
Mvycas <martynas.vycas@hotmail.com>
2022-12-05 22:28:51 +0100

    https://www.youtube.com/watch?v=dQw4w9WgXcQ

---
+++ Entity/Preset.cs
@@ -0,0 +1,13 @@
+﻿namespace Entity;
+
+public class Preset
+{
+    public string Id { get; set; } = RandomIDGenerator.Generate(20);
+    public string PresetName { get; set; } = "";
+    public float TemperatureMax { get; set; }
+    public float HumidityMax { get; set; }
+    public float Co2Max { get; set; }
+    public float TemperatureMin { get; set; }
+    public float HumidityMin { get; set; }
+    public float Co2Min { get; set; }
+}
