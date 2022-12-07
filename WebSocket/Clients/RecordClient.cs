05543bebc717e1967bdb22c9283def04bc9e2ffe
GoForMusic <militaruadriancristian@gmail.com>
2022-12-07 15:39:53 +0100

    ..

--- WebSocket/Clients/RecordClient.cs
+++ WebSocket/Clients/RecordClient.cs
@@ -2,8 +2,11 @@
 using System.Net.WebSockets;
 using System.Text;
 using System.Text.Json;
+using System.Text.Json.Nodes;
 using EFCDataBase.DAOImpl;
 using Entity;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using WebSocket.Interfaces;
 using WebSocket.Stream;
 
@@ -17,7 +20,7 @@ public class RecordClient : IWebClient
     
     
     private readonly string _uriAddress = "wss://iotnet.cibicom.dk/app?token=vnoUeAAAABFpb3RuZXQudGVyYWNvbS5kawhxYha6idspsvrlQ4C7KWA=";
-    private readonly string _eui = "0004A30B00219CAC";
+    private readonly string _eui = "0004A30B0021B92F";
 
     public RecordClient()
     {
@@ -28,23 +31,36 @@ public class RecordClient : IWebClient
     
     private Record? ReceivedData(string receivedJson)
     {
-        UpLinkStream? receivedPayload = JsonSerializer.Deserialize<UpLinkStream>(receivedJson);
+        //UpLinkStream? receivedPayload = JsonSerializer.Deserialize<UpLinkStream>(receivedJson,new JsonSerializerOptions()
+        //{
+        //    PropertyNameCaseInsensitive = true,
+        //    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        //});
 
-        Record? localRecord = null;
-        if (!String.IsNullOrEmpty(receivedPayload!.data))
-        {
-            Console.WriteLine(receivedPayload.data.ToString());
-            
-            int i = 0;
-            int l = 4;
-            double temperature = Math.Round(Convert.ToInt16(receivedPayload.data.Substring(i,l),16) / 10.0, 1);
-            double humidity = Math.Round(Convert.ToInt16(receivedPayload.data.Substring(i+1*l,l),16) / 10.0, 1);
-            double co2 = Convert.ToInt16(receivedPayload.data.Substring(i+2*l,l),16);
-            byte status = Convert.ToByte(receivedPayload.data.Substring(i+3*l,2),16);
-            localRecord = new Record((float)temperature, (float)humidity, (float)co2);
-        }
+        //var details = JObject.Parse(receivedJson);
+        //Console.WriteLine(JsonConverter(receivedJson,Inte));
+        Console.WriteLine(JsonConvert.SerializeObject(receivedJson, Formatting.Indented));
+        var details = JObject.Parse(receivedJson);
+        Console.WriteLine(details["data"]);
         
-        return localRecord;
+        //Console.WriteLine(receivedPayload.data.Substring(0));
+        //Record? localRecord = null;
+        //if (!String.IsNullOrEmpty(receivedPayload!.data))
+        //{
+         char[] array = details["data"].Value<String>().ToCharArray();
+         
+         double humidity = Convert.ToInt16(array[0].ToString()+array[1].ToString(),16);
+         humidity += Convert.ToInt16(array[2].ToString()+array[3].ToString(),16)/100f;
+         double temperature= Convert.ToInt16(array[4].ToString()+array[5].ToString(),16);
+         temperature+= Convert.ToInt16(array[6].ToString()+array[7].ToString(),16)/100f;
+         double co2 = Convert.ToInt16(array[8].ToString()+array[9].ToString(),16);
+         co2 += Convert.ToInt16(array[10].ToString()+array[11].ToString(),16)/100f;
+            //byte status = Convert.ToByte(receivedPayload.data.Substring(i+3*l,2),16);
+           // localRecord = new Record((float)temperature, (float)humidity, (float)co2);
+            Console.WriteLine(humidity + " " + temperature + " " + co2);
+        //}
+        
+        return null;
     }
     
     private async Task ConnectClientAsync()
@@ -70,21 +86,21 @@ public class RecordClient : IWebClient
                 cmd = "tx",
                 port = 2,
                 EUI = _eui,
-                data = "E803"
+                data = "001e001807d0"
             };
-            string payloadJson = JsonSerializer.Serialize(upLinkStream);
+            string payloadJson = JsonConvert.SerializeObject(upLinkStream);
             Console.WriteLine("---CALL-DownLink---");
             Console.WriteLine(payloadJson);
             Console.WriteLine("---CALL-DownLink---");
             //send
             await _clientWebSocket.SendAsync(Encoding.UTF8.GetBytes(payloadJson), WebSocketMessageType.Text, true, CancellationToken.None);
             
-            Byte[] buffer = new byte[256];
+            Byte[] buffer = new byte[500];
             var x = await _clientWebSocket.ReceiveAsync(buffer,CancellationToken.None);
             var strResult = Encoding.UTF8.GetString(buffer);
             
             Console.WriteLine("---Receive-DownLink---");
-            Console.WriteLine(strResult);
+            //Console.WriteLine(strResult);
             Console.WriteLine("---Receive-DownLink---");
             
             //get data and convert
